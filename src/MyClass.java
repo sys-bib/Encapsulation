a06b2148687d3dd13e766d36787c072b687dbc12
unknown <ahmadiburrahman21@gmail.com>
2022-01-21 17:00:01 +0700

    Initial commit

---
+++ src/MyClass.java
@@ -0,0 +1,8 @@
+public class MyClass {
+    public static void main(String[] args) {
+        MyPrivateClass myPrivateClass = new MyPrivateClass();
+        myPrivateClass.setStr("update my string");
+        myPrivateClass.printString();
+        //System.out.println(myPrivateClass.getStr());
+    }
+}
---
+++ src/MyPrivateClass.java
@@ -0,0 +1,19 @@
+public class MyPrivateClass {
+    private static String str = "My String";
+
+    private String getStr(){
+        return str;
+    }
+
+    public void setStr(String str){
+        this.str = str;
+    }
+
+    public void printString(){
+        System.out.println(str);
+    }
+
+    private void myMethod(){
+        System.out.println("my method");
+    }
+}
